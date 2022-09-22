5db755f25f4f8ff0272caedec3d6a3532de27903
mahesh <maheshb@gaussiansolutions.com>
2022-09-22 13:22:50 +0530

    packaging config changes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import { Logger, Queue } from './core';
+import { Config } from './models/config';
 
 export { Logger }
 export { Queue }
+
+export class TDEICore {
+
+
+    initialize(config: Config) {
+        switch (config.provider) {
+            case 'Azure':
+                
+                break;        
+            default:
+                break;
+        }
+    }
+}
