d42298dcef93a48e1bdc172828edf29371d4d8ff
Naresh Kumar D <susrisha@gmail.com>
2022-11-09 10:19:29 +0530

    Update example.ts
    Removed sensitive information

--- src/example.ts
+++ src/example.ts
@@ -6,25 +6,6 @@ import { AuditRequest } from "./core/logger/model/audit_request";
 import { CoreConfig } from "./models/config";
 
 
-// let configuration = Config.from({
-//     provider:'Azure', // default
-//     cloudConfig:{
-//         connectionString:{
-//             appInsights:"InstrumentationKey=f98ba6d5-58e1-4267-827e-ccac68caf50f;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/",
-//             serviceBus:"Endpoint=sb://tdei-sample.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=4UNDrVpThcnbqWlGFFQEcivuPlvMMWcSHwbyHgEv+rg="
-//         },
-//         logQueue:'tdei-ev-logger'
-//     }
-// });
-
-// connections:{
-//     serviceBus: "Endpoint=sb://tdei-sample.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=4UNDrVpThcnbqWlGFFQEcivuPlvMMWcSHwbyHgEv+rg=",
-//     blobStorage:"DefaultEndpointsProtocol=https;AccountName=tdeisamplestorage;AccountKey=l9JSJCGq9NrXqRVKApSk1wwV27aWaOVuxeY0NWOZz2svIlJzyncr3UFTzLoAanFbmJIeb2WmwIcS+AStj5gELg==;EndpointSuffix=core.windows.net",
-//     appInsights:"InstrumentationKey=f98ba6d5-58e1-4267-827e-ccac68caf50f;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/"
-// },
-// queueName:"tdei-poc-queue",
-// blobContainerName:"tdei-storage-test",
-// appName: process.env.npm_package_name
 // Get the dotenv things
 require('dotenv').config()
 
