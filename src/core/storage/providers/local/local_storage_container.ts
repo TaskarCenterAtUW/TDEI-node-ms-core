ff93bdfd9936780795e1b543bec31c0220d10791
Naresh Kumar D <susrisha@gmail.com>
2022-12-15 12:29:42 +0530

    file listing is done
    Listing of files from the remote setup is done

---
+++ src/core/storage/providers/local/local_storage_container.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { FileEntity } from "../../abstract/file_entity";
+import { StorageContainer } from "../../abstract/storage_container";
+import { LocalFileEntity } from "./local_file_entity";
+
+type FileResponse = {
+    isDirectory: boolean,
+    name:string,
+    path: string,
+    mimeType: string
+}
+type FileListResponse = FileResponse[];
+// Represents a single root folder
+export class LocalStorageContainer implements StorageContainer {
+
+    name: string;
+
+    excludedFiles:string[] = ['.DS_Store'];
+
+    listFiles(): Promise<FileEntity[]> {
+        return new Promise(async (resolve,reject)=>{
+                var url = 'http://localhost:8080/files/list/'+this.name+'/';
+                axios.get<FileResponse[]>(url).then((response)=>{
+                    // console.log(response.data);
+                    // var firstResponse = response.data[0];
+                    // console.log(firstResponse);
+                    const files: FileEntity[] = [];
+                    response.data.forEach((singleResponse)=>{
+                        if(this.excludedFiles.indexOf(singleResponse.name) == -1){ // Not in any of the excluded files
+                        files.push(new LocalFileEntity(singleResponse.path,singleResponse.mimeType));
+                        }
+                    });
+                    resolve(files);
+                    
+                }).catch((e)=>{
+                    reject(e);
+                });
+                
+        });
+    }
+    createFile(name: string, mimeType: string): FileEntity {
+        throw new Error("Method not implemented.");
+    }
+
+    constructor(name: string) {
+        this.name = name;
+     }
+
+}
