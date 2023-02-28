d77d43c965a15b600e0e2f244761792b396257e0
priya-badmanji <priyadarshini.badmanji@gmail.com>
2023-02-28 15:42:44 +0530

    auth simulation improvement

--- src/core/auth/provider/simulated/simulated_authorizer.ts
+++ src/core/auth/provider/simulated/simulated_authorizer.ts
@@ -2,29 +2,31 @@ import { IAuthConfig } from "../../abstracts/IAuthConfig";
 import { IAuthorizer } from "../../abstracts/IAuthorizer";
 import { PermissionRequest } from "../../model/permission_request";
 
+const USER_ID_AUTH_FALSE: string = "000000-0000-0000-0000-000000";
 /**
  * Simulates the authorization.
  * Authorization is based on the `shouldSatisfyAll` parameter
  * passed in the permission request
  */
-export class SimulatedAuthorizer implements IAuthorizer{
+export class SimulatedAuthorizer implements IAuthorizer {
 
     /**
      * Simulated Authorizer. Does simulated check for authorization.
      * @param config Partial configuration with provider as `Simulated`
      */
-    constructor(config:Partial<IAuthConfig>) {
+    constructor(config: Partial<IAuthConfig>) {
         // Do nothing
     }
     /**
      * 
      * @param permissionRequest Permission request instance
+     * User id with "000000-0000-0000-0000-000000" will simulate result to false else true otherwise.
      * @returns Promise<boolean> or an exception
      */
     hasPermission(permissionRequest: PermissionRequest): Promise<boolean> {
-        return new Promise((resolve,reject)=>{
-            resolve(permissionRequest.shouldSatisfyAll);
+        return new Promise((resolve, reject) => {
+            resolve(permissionRequest.userId === USER_ID_AUTH_FALSE ? false : true);
         });
     }
-    
+
 }
