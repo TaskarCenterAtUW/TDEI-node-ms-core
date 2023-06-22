be5227200a247cb5ea94072d3bf3af16752f6d9a
Naresh Kumar D <susrisha@gmail.com>
2023-06-22 14:43:51 +0530

    Update azure-service-bus-topic.unit.ts
    Local topic unit testing included

--- test/azure-service-bus-topic.unit.ts
+++ test/azure-service-bus-topic.unit.ts
@@ -5,6 +5,8 @@ import { Topic } from "../src/core/queue/topic";
 import { ITopicSubscription } from "../src/core/queue/abstracts/IMessage-topic";
 import { QueueMessage } from "../src/core/queue";
 import { BadRequestResourceError } from "../src/utils/resource-errors/bad-request-resource-error";
+import { LocalQueueConfig } from "../src/core/queue/providers/local/local-queue-config";
+import { LocalTopic } from "../src/core/queue/providers/local/local-topic";
 
 const fakePublish = jest.fn();
 const fakeSubscribe = jest.fn();
@@ -20,6 +22,21 @@ jest.mock('../src/core/queue/providers/azure-service-bus-topic', () => {
       })
     };
   });
+
+  const fakeLocalPublish = jest.fn();
+  const fakeLocalSubscribe = jest.fn();
+
+  jest.mock('../src/core/queue/providers/local/local-topic', () =>{
+    return {
+        LocalTopic : jest.fn().mockImplementation(() => {
+            
+            return {
+                subscribe: (subscription: string, handler: ITopicSubscription): Promise<void> => { fakeLocalSubscribe(); return Promise.resolve() },
+                publish : (message: QueueMessage): Promise<void> => { fakeLocalPublish(); return Promise.resolve();}
+            };
+          })
+    }
+  })
 
 describe('Azure service bus topic', ()=>{
     const configuration = AzureQueueConfig.default();
@@ -40,6 +57,62 @@ describe('Azure service bus topic', ()=>{
         
         expect(topic).toBeInstanceOf(Topic);
         expect(AzureServiceBusTopic).toHaveBeenCalledTimes(1);
+    })
+
+    it('Should send message via ServiceBus when message is sent', () => {
+
+        const topic = Core.getTopic('sampletopic',configuration);
+       
+        topic.publish(queuemessage);
+        expect(fakePublish).toHaveBeenCalledTimes(1);
+
+    })
+
+    it('Should listen to the said subscription appropriately', () => {
+        const topic = Core.getTopic('sampletopic',configuration);
+
+        topic.subscribe('sample-subscription', {
+            onReceive(message:QueueMessage){},
+            onError(error:Error){}
+        });
+        expect(fakeSubscribe).toHaveBeenCalledTimes(1);
+    })
+
+    it('Should throw error when the client is not available', async () =>{
+
+        const topic = Core.getTopic('sampletopic',configuration);
+        // force assigning null for client
+        topic.client = undefined;
+       const result = topic.subscribe('sample-subscription', {
+            onReceive(message:QueueMessage){},
+            onError(error:Error){}
+        })
+        const publishResult = topic.publish(queuemessage);
+        await expect(result).rejects.toThrow(BadRequestResourceError);
+        await expect(publishResult).rejects.toThrow(BadRequestResourceError);
+
+    })
+
+})
+
+describe('Local topic', ()=>{
+    const configuration = LocalQueueConfig.default();
+    const publishedDate = new Date(2022,5,18);
+    const queuemessage = QueueMessage.from({
+        message:'Sample message',
+        messageId:'123',
+        messageType:'sample-event',
+        publishedDate: ('{publishedDate}')
+    });
+
+    it('Should have provider as Local', () => {
+        expect(configuration.provider).toEqual("Local");
+    })
+    it('Should get the service topic', () => {
+        const topic = Core.getTopic('sampletopic',configuration);
+        
+        expect(topic).toBeInstanceOf(Topic);
+        expect(LocalTopic).toHaveBeenCalledTimes(1);
     })
 
     it('Should send message via ServiceBus when message is sent', () => {
