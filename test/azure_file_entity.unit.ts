aa70e9ca640a13d35af33a69cbb64869f2a5cad9
Naresh Kumar D <susrisha@gmail.com>
2023-06-27 14:05:51 +0530

    Linting done appropriately
    Linting done

--- test/azure_file_entity.unit.ts
+++ test/azure_file_entity.unit.ts
@@ -11,59 +11,64 @@ import { Readable } from "stream"
 const mockDownload = jest.fn();
 const mockUpload = jest.fn();
 
-jest.mock('@azure/storage-blob', ()=>{
+jest.mock('@azure/storage-blob', () => {
     return {
-        BlockBlobClient : jest.fn().mockImplementation(()=>{
+        BlockBlobClient: jest.fn().mockImplementation(() => {
             return {
-            download:(offset?: number, count?: number, options?: BlobDownloadOptions): Promise<{readableStreamBody:Readable}> => {
-                const mockedStream = Readable.from('random-data', {encoding: 'utf8'})
-                // mockedStream._read = function () { /* do nothing */ };
-                // mockedStream.push('random-data');
-                mockDownload();
-                return Promise.resolve({
-                    readableStreamBody: mockedStream
-                })
-            },
+                download: (offset?: number, count?: number, options?: BlobDownloadOptions): Promise<{ readableStreamBody: Readable }> => {
+                    const mockedStream = Readable.from('random-data', { encoding: 'utf8' })
+                    mockDownload();
+                    return Promise.resolve({
+                        readableStreamBody: mockedStream
+                    })
+                },
 
-            uploadData: (data: Buffer | Blob | ArrayBuffer | ArrayBufferView, options?: BlockBlobParallelUploadOptions):  Promise<BlobUploadCommonResponse> => {
-                return mockUpload();
+                uploadData: (data: Buffer | Blob | ArrayBuffer | ArrayBufferView, options?: BlockBlobParallelUploadOptions): Promise<BlobUploadCommonResponse> => {
+                    return mockUpload();
+                }
             }
-        }
         })
     }
 })
 
-describe('Azure file entity', ()=>{
-    it('Should be able to initialize', ()=>{
-        const file = new AzureFileEntity('snfds',new BlockBlobClient(''))
+describe('Azure file entity', () => {
+    it('Should be able to initialize', () => {
+        
+        const file = new AzureFileEntity('snfds', new BlockBlobClient(''));
+
         expect(file).toBeTruthy();
     })
-    it('Should be able to download when the file stream is needed', async () =>{
+    it('Should be able to download when the file stream is needed', async () => {
 
-        const file = new AzureFileEntity('snfds',new BlockBlobClient(''))
+        const file = new AzureFileEntity('snfds', new BlockBlobClient(''));
+
         const stream = await file.getStream();
+        
         expect(mockDownload).toHaveBeenCalledTimes(1);
 
     })
 
-    it('Should be able to get the text', async ()=>{
+    it('Should be able to get the text', async () => {
+        
         mockDownload.mockClear();
-        const file = new AzureFileEntity('snfds',new BlockBlobClient(''))
+        const file = new AzureFileEntity('snfds', new BlockBlobClient(''))
+        
         const body = await file.getBodyText();
-        // console.log('Body is '+body);
+
         expect(body).toBe('random-data');
         expect(mockDownload).toHaveBeenCalledTimes(1);
 
     })
-    it('Should be able to upload the content from readable stream', async ()=>{
+    it('Should be able to upload the content from readable stream', async () => {
 
-        const dummyStream =  Readable.from([],{encoding:'binary'}); // Blank stream
-       
-        const file = new AzureFileEntity('snfds',new BlockBlobClient(''))
-       const uploadedEntity = await file.upload(dummyStream);
-       expect(mockUpload).toHaveBeenCalledTimes(1);
-       expect(uploadedEntity).toBe(file);
+        const dummyStream = Readable.from([], { encoding: 'binary' }); // Blank stream
+        const file = new AzureFileEntity('snfds', new BlockBlobClient(''))
+        
+        const uploadedEntity = await file.upload(dummyStream);
+
+        expect(mockUpload).toHaveBeenCalledTimes(1);
+        expect(uploadedEntity).toBe(file);
 
     })
-    
+
 })
