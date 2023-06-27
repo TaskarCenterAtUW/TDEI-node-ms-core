7e967dbc0ef31d0935d66ec7bbe57f2b3c782b69
Naresh Kumar D <susrisha@gmail.com>
2023-06-27 10:40:54 +0530

    Create azure_analytic.unit.ts
    Logger analytic added.

---
+++ test/azure_analytic.unit.ts
@@ -0,0 +1,50 @@
+/**
+ * Unit tests for Azure analytic `azure_analytic.ts`.
+ * This depends on Queue object and has two methods 
+ * - add
+ * - send
+ */
+
+import { Core } from "../src/core";
+import { AzureAnalytic } from "../src/core/logger/providers/azure_analytic";
+import { QueueMessage } from "../src/core/queue";
+
+// jest.mock('../src/core', ()=>{
+//     return {
+//     Core: jest.fn().mockImplementation(()=>{
+//     return {
+//         getQueue:jest.fn().mockImplementation(()=>{
+//             return {
+//                 add:(message:QueueMessage)=>{},
+//                 send:()=>{}
+//             }
+//         })
+//     }
+// })
+//     }
+// })
+const mockSend = jest.fn();
+const mockAdd = jest.fn();
+const mockGetQueue = jest.fn().mockImplementation(()=>{
+    return {
+        add:(message:QueueMessage)=>{mockAdd();},
+        send:()=>{mockSend();}
+    }
+});
+
+describe('Azure analytic', ()=>{
+    beforeAll(()=>{
+        Core.getQueue = mockGetQueue;
+    })
+    it('Should initialize with queue name', ()=>{
+        const analytic = new AzureAnalytic('sample');
+        expect(analytic).toBeTruthy();
+    })
+    it('Should be able to send message', ()=>{
+        const analytic = new AzureAnalytic('sample');
+        const messageToDeliver = {type:'sample'};
+        analytic.record(messageToDeliver)
+        expect(mockSend).toHaveBeenCalledTimes(1);
+        expect(mockAdd).toHaveBeenCalledTimes(1);
+    })
+})
